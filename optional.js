5ebcb568d2fc12dd190033ed62c80ae3280a6ad4
alindaByamukama <alindabyamukama@gmail.com>
2022-05-20 10:44:34 +0300

    JS: created get quote async function to fetch quotes from api w/ backup api cors-anywhere(temporary access) and error log

---
+++ optional.js
@@ -0,0 +1,17 @@
+
+// get quote from API
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+    try {
+        const response = await fetch(proxyUrl + apiUrl)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        // getQuote()
+        console.log('whoops, no quote! ', error)
+    }
+}
+
+// on load
+getQuote()
