a8a859c98254904af66553e22665ca317ecf8280
alindaByamukama <alindabyamukama@gmail.com>
2022-05-20 11:19:08 +0300

    JS: dom manipulation - replace missing author w/ unknown - reduce font size for long quotes - tweet quote function - new quote and twitter btn event listeners

--- optional.js
+++ optional.js
@@ -1,3 +1,11 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader');
+
+loader.hidden = true
 
 // get quote from API
 async function getQuote() {
@@ -6,12 +14,36 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl)
         const data = await response.json()
-        console.log(data)
+        // replace missing author with unknown
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+        // reduce font size for long quotes
+        if (data.quoteTet.length > 120) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText
     } catch (error) {
         // getQuote()
-        console.log('whoops, no quote! ', error)
+         console.log('whoops, no quote! ', error)
     }
 }
+
+// tweet quote function
+function tweetQuote() {
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank')
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // on load
 getQuote()
