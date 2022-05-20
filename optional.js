7e9044d03abe8bf26ad5117982a6ef4d99e4e832
alindaByamukama <alindabyamukama@gmail.com>
2022-05-20 11:38:53 +0300

    CodeReview: code-readability changed loading function names. errors - switched position of getQuote function and commented it due to api request overload

--- optional.js
+++ optional.js
@@ -5,14 +5,25 @@ const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
 const loader = document.getElementById('loader');
 
-loader.hidden = true
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function completeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // get quote from API
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    showLoadingSpinner()
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
     try {
-        const response = await fetch(proxyUrl + apiUrl)
+        const response = await fetch(apiUrl)
         const data = await response.json()
         // replace missing author with unknown
         if (data.quoteAuthor === '') {
@@ -27,9 +38,11 @@ async function getQuote() {
             quoteText.classList.remove('long-quote')
         }
         quoteText.innerText = data.quoteText
+        completeLoadingSpinner()
     } catch (error) {
-        // getQuote()
-         console.log('whoops, no quote! ', error)
+         console.log('No quote? ', error)
+        // getQuote() // 429 (too many requests)
+
     }
 }
 
